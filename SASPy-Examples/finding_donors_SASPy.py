b97b88665ec83a9e9e2a716bfc15aeea04f85677
ngrajales1 <15281436+ngrajales1@users.noreply.github.com>
2019-03-12 16:10:32 -0500

    currently can not use .head with SAS object. Display does show data.

--- SASPy-Examples/finding_donors_SASPy.py
+++ SASPy-Examples/finding_donors_SASPy.py
@@ -10,20 +10,20 @@ from time import time
 from IPython.display import display
 
 # starting the SAS session
-sas = saspy.SASsession(cfgname='default')
+sas = saspy.SASsession(cfgname='autogen_winlocal')
 
 #loading the census data.
 
 #The foloowing method below display how to read a csv file using pandas and then reading in
 #the data frome into a SAS object
 cen_data0_pd = pd.read_csv("C:\\Users\\negraj\\Documents\\Learning\\SAS and SASPy\\SASPy-Examples\\census.csv")
-cen_data0 = .df2sd(cen_data0_pd) # the short form of: hr = sas.dataframe2sasdata(hr_pd)
+cen_data0 = sas.df2sd(cen_data0_pd) # the short form of: hr = sas.dataframe2sasdata(hr_pd)
 
 #You can also read in a dataset directly into a SAS object using the code below.
 #cen_data0 = sas.read_csv("C:\\Users\\negraj\\Documents\\Learning\\SAS and SASPy\\SASPy-Examples\\census.csv")
 
 #Display the first record
-#display(cen_data0_pd.head(n=1))
-cen_data = cen_data0.head(n=1)
+display(cen_data0_pd.head(n=1))
+cen_data0.head()
 
 print("End of program")
